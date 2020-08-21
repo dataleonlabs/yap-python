efde4711707a61fbf2eac45a64cec1efd27ad576
Gérard TOKO <gerard@youngapp.co>
2020-08-21 13:31:54 +0200

    feat: add devops informations

--- yapsdk/Yap.py
+++ yapsdk/Yap.py
@@ -15,14 +15,7 @@
 
 import requests
 import json
-import base64
-
-
-def isBase64(s):
-    try:
-        return base64.b64encode(base64.b64decode(s)) == s
-    except Exception:
-        return False
+from yapsdk.utils import isBase64
 
 
 class Yap:
