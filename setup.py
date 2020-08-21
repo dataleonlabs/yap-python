e5f1d9b8f8313f70a1935c55d5a75701a4350c17
Gérard TOKO <gerard@youngapp.co>
2020-08-21 14:35:00 +0200

    chore: update .github

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'requests >= 2.20; python_version >= "3.0"',
         'requests[security] >= 2.20; python_version < "3.0"',
     ],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
     project_urls={
         "Bug Tracker": "https://github.com/youngapp/yap-python/issues",
         "Documentation": "https://developer.youngapp.co",
